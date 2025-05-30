bb1bdab67834bb3afce844d35b0ac18c3e4f609e
Mahaub Alam <mahabubalom72091@gmail.com>
2025-05-30 20:50:16 +0300

    added some code

--- modeul13/fs.js
+++ modeul13/fs.js
@@ -1,3 +1,18 @@
 // 1.syncronus
+  //1.file read  I/O intr
 
 // 2.asynctonus
+
+const fs = require("fs");
+console.log('task 1')
+
+
+const text = " i will be coming fullsatc developer insahallha"
+fs.writeFileSync("./hellow.txt", text);
+
+console.log("tesk3")
+
+const data = fs.readFileSync("./hellow.txt", { encoding: "utf-8" })
+
+console.log('task4')
+console.log(data)
