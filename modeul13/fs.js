4c9a3a47787856fc7000e30ce5f69bf2478a172a
Mahaub Alam <mahabubalom72091@gmail.com>
2025-05-31 16:22:32 +0300

    ADDED

--- modeul13/fs.js
+++ modeul13/fs.js
@@ -8,11 +8,15 @@ console.log('task 1')
 
 
 const text = " i will be coming fullsatc developer insahallha"
-fs.writeFileSync("./hellow.txt", text);
 
-console.log("tesk3")
+fs.readFile("./hellow.txt", { encoding: "utf-8" }, (err, data) => {
+    if (err) {
+        console.log("something went wrong", err)
+        return;
+    }
+    console.log(data)
+});
 
-const data = fs.readFileSync("./hellow.txt", { encoding: "utf-8" })
 
-console.log('task4')
-console.log(data)
+
+// console.log(data)
